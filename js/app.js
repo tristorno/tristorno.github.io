bbdf791963bd7498cfa8c0740b19dbf469ae5c21
tristorno <101147127+tristorno@users.noreply.github.com>
2025-02-03 20:54:39 +0300

    new version

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function appWrite() {
+    var mess = document.getElementById("msg").value;
+    document.getElementById("msg_new").innerText=mess;
+    document.getElementById("msg").value='';
+//console.log(123);
+}
